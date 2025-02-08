79f0af8749e2f861846352017bd5fcf1b7f14fcd
Aldio Yohanes <aldioguire@gmail.com>
2025-02-08 21:16:34 +0700

    Update auth-header, footer, dan tambah gambar dinaspendidikan

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,6 +1,6 @@
 <footer class="main-footer">
     <div class="footer-left">
-        Copyright &copy; 2024 <div class="bullet">
+        Copyright &copy; 2025 <div class="bullet">
     <div class="footer-right">
         Dinas Pendidikan Riau
     </div>
